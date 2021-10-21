07e40de34a65cd1820c25cf0b1b43cca9d2f2764
github username <kelly.hesterly@gmail.com>
2021-10-20 21:48:21 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,12 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def active
+    @student = Student.find(params[:id])
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
+  end
   private
 
     def set_student
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get 'student/:id', to: 'students#active', as: 'activate'
   resources :students
 end
