3e0cc9b9c78cff0d8e53da5f695c4300a3f85c99
github username <kelly.hesterly@gmail.com>
2022-01-12 19:52:42 -0700

    commit tree 1106eac7d6f94170cb7f14c0cb0a46ccfdb7f723

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'student/:id', to: 'students#active', as: 'activate'
+  get 'student/:id', to: 'students#activate', as: 'activate_student'
   resources :students
 end
